e3bb23f67d30c5867f5aca4f6dda5d9b5186da80
Nikhil Mone <nikhilmone7@gmail.com>
2013-05-05 19:47:11 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,58 @@
+package com.test;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable {
+
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private long id;
+	private String username;
+	private String password;
+	private String email;
+	private Date birthdate;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+}
